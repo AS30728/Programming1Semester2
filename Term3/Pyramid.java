f7e9f094c0b30c73a4afcf3072e3bb8074fb72a7
Andy Sudarmadi <andys30728@gmail.com>
2016-02-10 21:30:40 -0700

    Create Pyramid.java

---
+++ Term3/Pyramid.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Pyramid{
+  public static void main(String args[]){
+    Scanner scn = new Scanner(System.in);
+    double l;
+    double w;
+    double h;
+    System.out.println("Calculate the surface area & volume of a rectangular pyramid.");
+    System.out.println("Length:");
+    l = scn.nextDouble();
+    System.out.println("Width:");
+    w = scn.nextDouble();
+    System.out.println("Height:");
+    h = scn.nextDouble();
+    System.out.println("Volume: " + (l*w*h)/3 );
+    /*System.out.println("Surface Area: " + 2*(w*l+h*l+h*w));*/
+  } 
+}
