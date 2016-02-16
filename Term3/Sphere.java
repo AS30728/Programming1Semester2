aae9d229cc0baafa5adc2eb5c23ef6305fcaf320
Andy Sudarmadi <andys30728@gmail.com>
2016-02-16 13:05:30 -0700

    Update Sphere.java

--- Term3/Sphere.java
+++ Term3/Sphere.java
@@ -7,12 +7,7 @@ public class Sphere{
     System.out.println("Calculate the surface area & volume of a sphere.");
     System.out.println("Radius:");
     r = scn.nextDouble();
-    /*System.out.println("Width:");
-    w = scn.nextDouble();
-    System.out.println("Height:");
-    h = scn.nextDouble();
-    System.out.println("Volume: " + l*w*h);
-    System.out.println("Surface Area:" + 2*(w*l+h*l+h*w));*/
+    System.out.println("Volume: " + (1.33333 * Math.PI * r * r * r));
     System.out.println("Volume: " + (4 * Math.PI * r * r));
   } 
 }
